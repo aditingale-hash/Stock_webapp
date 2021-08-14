b558020fef111124451731170bda3cfa0d696c56
aditingale-hash <71597160+aditingale-hash@users.noreply.github.com>
2021-08-14 16:19:14 +0530

    Update Stock.py

--- Stock.py
+++ Stock.py
@@ -3,11 +3,11 @@ import streamlit as st
 import pandas as pd
 from datetime import date, timedelta
 from datetime import date
-quandl.ApiConfig.api_key = "_4zc86ZxsiJzYAbrG7xt"    
+quandl.ApiConfig.api_key = "_4zc86ZxsiJzYAbrG7xt"   
 
 symbol= {} #creating a dict
 today = str(date.today()) # todays date
-with open("./symbol.txt") as f:
+with open("./symbols.txt") as f:
     for line in f:
         (key, val) = line.split("|")
         symbol[(key)] = val.rstrip()
@@ -18,22 +18,36 @@ with open("./symbol.txt") as f:
 # and adding to symbol like this key have this value
 
 
-st.sidebar.header("**stock Symbol**")
-stock = st.sidebar.selectbox(
-    'which stocks you have',
-  symbol.keys()) # displaing only keys names of stock
+st.sidebar.header("*Stock Symbol*")
+with st.sidebar.form('Form1'):
+  stock = st.selectbox('wWich stocks you have', symbol.keys())
+  st.form_submit_button('Submit')
+# stock = st.sidebar.selectbox(
+#     'which stocks you have',
+#   symbol.keys()) # displaing only keys names of stock
 ticker = symbol[stock] # getting back the code of stock by stock name
 today_1 = date.today()
 start = today_1 - timedelta(days=today_1.weekday())
 end = start - timedelta(days=6)
-data_normal = quandl.get("BSE/"+ticker, start_date=end, end_date=today_1) # libraby have this function which takes start and end with code
+data_normal = quandl.get("BSE/"+ticker, start_date=end, end_date=today_1) 
+# libraby have this function which takes start and end with code
+# data_normal[open] = data_normal[open].astype(float) * 10.00
+
 data = data_normal.T
 
 st.title(f'{stock} portfolio')
-st.write(data.loc[:,::-1]) #display data  now we can disply data like last 5 days so we can plot graph your turn
-open = data_normal['Open']
-close = data_normal['Close']
-Low = data_normal['Low']
-High = data_normal['High']
-bar_grpah_disply_open_Close = pd.DataFrame(data_normal,columns=['Open', 'Close', 'Low', 'High'])
-st.bar_chart(bar_grpah_disply_open_Close)
+
+st.write(data.loc[:,::-1]) 
+int_val = 1
+with st.form(key='my_form'):
+  int_val = st.number_input(label= 'How many stocks you have', value=1, step=1)
+  submit_button = st.form_submit_button(label='Calculate')
+#display data  now we can disply data like last 5 days so we can plot graph your turn
+if(int_val > 1):
+  data_normal['Open'] = data_normal['Open'].apply(lambda x: x*int_val)
+  data_normal['Close'] = data_normal['Close'].apply(lambda x: x*int_val)
+  data_normal['High'] = data_normal['High'].apply(lambda x: x*int_val)
+  data_normal['Low'] = data_normal['Low'].apply(lambda x: x*int_val)
+  newData = data_normal.T
+  # newData = pd.DataFrame(newData,columns=['Open','Close','High', 'Low'])
+  st.write(newData.loc[:,::-1]
