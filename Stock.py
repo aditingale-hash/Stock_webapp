4944c259ea96fe83fce26f1a5526666b615e5b80
aditingale-hash <71597160+aditingale-hash@users.noreply.github.com>
2021-08-14 16:43:57 +0530

    Update Stock.py

--- Stock.py
+++ Stock.py
@@ -43,6 +43,8 @@ st.sidebar.header("*Enter your Stock*")
 with st.sidebar.form(key='my_form'):
   int_val = st.number_input(label= 'How many stocks you have', value=1, step=1)
   submit_button = st.form_submit_button(label='Calculate')
+    
+st.title(f'{stock} cureent price of your stock')
 #display data  now we can disply data like last 5 days so we can plot graph your turn
 if(int_val > 1):
   data_normal['Open'] = data_normal['Open'].apply(lambda x: x*int_val)
